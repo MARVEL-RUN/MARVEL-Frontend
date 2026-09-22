81dee16322c2fe792445934f514126f96f5501ec
eunjin <huhej0315@gmail.com>
2026-09-22 10:31:18 +0900

    feat: #167 팝업 이미지 변경 및 url 추가

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -16,7 +16,7 @@ export const MAIN_ASSETS = {
   hero: "/images/main/hero-characters.png",
   sideBanner: "/images/main/sidebanner.png",
   sideBannerMobile: "/images/main/sidebanner-mobile.png",
-  popupShuttleSurvey: "/images/main/popup/shuttle-survey.jpeg",
+  popupShuttleSurvey: "/images/main/popup/shuttle-survey-v2.jpeg",
   footerHost: "/images/main/footer/hosted.png",
   footerOrganizer: "/images/main/footer/organizer.png",
   introMarvel: "/images/main/intro-marvel.svg",
